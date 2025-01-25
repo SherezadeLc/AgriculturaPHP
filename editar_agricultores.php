eb05eb80430a1caa2f1dcdeb770ce1798a97c1a1
Sherezade <Sherezade@172.16.50.152>
2025-01-25 17:38:11 +0100

    menu terminado+clases

--- editar_agricultores.php
+++ editar_agricultores.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+session_start();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +9,33 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        // Conectar con el servidor de base de datos
+        $conexion = mysqli_connect("localhost", "root", "","agricultura")
+                or die("No se puede conectar con el servidor");
+        
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            @$id = $_POST['id_agricultor'];
+            @$nombre = $_POST['nombre'];
+            @$dni = $_POST['dni'];
+            @$estado = $_POST['estado'];
+            @$contrasena=$_POST['contrasena'];
+
+            $consulta = "UPDATE agricultores SET nombre='$nombre', telefono='$telefono', email='$email' WHERE id='$id'";
+            if (mysqli_query($conexion, $query_update)) {
+                echo "<p>El agricultor ha sido actualizado correctamente.</p>";
+            } else {
+                echo "<p>Error al actualizar el agricultor: " . mysqli_error($conexion) . "</p>";
+            }
+        }
+        ?>
+        
+        <form action="menu.php" method="POST">
+            <input type="submit" name="volver" value="Volver"><br>
+        </form><br>
+        <?php
+        // Cerrar conexión
+        mysqli_close($conexion);
         ?>
     </body>
+
 </html>
